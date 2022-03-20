5d7a27310d024998ed1f4bdb5fbaabfb20682f8b
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-03-20 16:59:30 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./components/navbar";
+import {Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar/>
+
+
+    </div>
+  );
+}
+
+export default App;
